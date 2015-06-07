c2d0aab4fd494038d8edd19cd5583646077f1921
AmyDZeng <amy.d.zeng@gmail.com>
2015-06-07 14:08:04 -0400

    Added Java Files
    Added Block, LiveBlock, Location and MainFrame java files.

---
+++ Block.java
@@ -0,0 +1,15 @@
+//TODO: Document class
+public class Block {
+	static int col;
+	
+	public int getCol(){
+		return Block.col;
+	}
+	
+	public void setCol(int col){
+		Block.col = col;
+	}
+
+}
+
+
---
+++ LiveBlock.java
@@ -0,0 +1,9 @@
+public class LiveBlock extends Block{
+	static Location xy;
+	//TODO: code this thing
+	Location getLoc(){;
+	void fall();
+	void rotate(Location Control);
+	void rotateInv(Location Control);
+	Block settle();
+}
