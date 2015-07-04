a0528666e7dadce88417282050334f3b52164f6c
YuFuTotG <kevin435308@hotmail.com>
2015-07-04 12:10:55 -0400

    Clean Repository

--- LiveBlock.java
+++
@@ -1,9 +0,0 @@
-public class LiveBlock extends Block{
-	static Location xy;
-	//TODO: code this thing
-	Location getLoc(){;
-	void fall();
-	void rotate(Location Control);
-	void rotateInv(Location Control);
-	Block settle();
-}
